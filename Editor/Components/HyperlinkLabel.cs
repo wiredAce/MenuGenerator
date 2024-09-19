c29ddee6c18e6f4ec5161b25798b3889c4e18114
m.brombeis <m.brombeis@bermuc.de>
2024-09-19 20:19:05 +0200

    Applied OOP principles to EditInput.cs and commented

--- Editor/Components/HyperlinkLabel.cs
+++ Editor/Components/HyperlinkLabel.cs
@@ -3,6 +3,9 @@ using UnityEngine.UIElements;
 
 public class HyperlinkLabel : Label
 {
+    /// <summary>
+    /// Generate a clickable label that opens a URL
+    /// </summary>
     public HyperlinkLabel(string text, string url) : base(text)
     {
         AddToClassList("hyperlink");
